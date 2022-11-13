c14efb2455b29afd7ba59f34f512b3827fd8806c
unknown <avir4m@gmail.com>
2022-11-13 20:53:03 +0200

    css/html fixed

--- src/Website/static/js/index.js
+++ src/Website/static/js/index.js
@@ -1,20 +1,3 @@
-function showPassword(number) {
-    if (number === 0) {
-      const input = document.getElementById(`password`);
-      const img = document.getElementById(`icon-eye`)
-    } else {
-      const input = document.getElementById(`password${number}`);
-      const img = document.getElementById(`icon-eye${number}`)
-    }
-    if (input.type === "password") {
-      input.type = "text";
-      img.src = "/static/images/icons/eye.png";
-    } else {
-      input.type = "password";
-      img.src = "/static/images/icons/eye-crossed.png";
-    }
-}
-
 function alertClose() {
   const messages = document.querySelector(".messages");
   messages.remove();
